d9111a6c9a7809707447c23a8976c83ce4ee3bb6
brendanjcrowley <brendanjcrowley@mycit.ie>
2019-10-09 15:17:16 +0100

    Completed Universal Crossover function

--- assignment-1/TSP_00140363.py
+++ assignment-1/TSP_00140363.py
@@ -81,28 +81,10 @@ class BasicTSP:
         pass
 
     def uniformCrossover(self, indA, indB):
-        """Executes a uniform crossover and returns a new individual
-        :param ind1: The first parent (or individual)
-        :param ind2: The second parent (or individual)
-        :returns: A new individual"""
-
-        child = Individual(self.genSize)
-
-        # For each gene point:
-        for i in range(0, self.genSize):
-
-            # Generate random int (1 or 2)
-            parent_choice = random.randint(1,2)
-
-            # If random int is 1, child inherits Parent1's ith gene
-            if parent_choice == 1:
-                child.genes[i] = indA.genes[i]
-
-            # else (if random int is 2), child inherits Parent2's ith gene
-            else:
-                child.genes[i] = indA.genes[i]
-                
-        return child
+        """
+        Your Uniform Crossover Implementation
+        """
+        pass
 
     def pmxCrossover(self, indA, indB):
         """
